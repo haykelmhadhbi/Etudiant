2dce2a9a58078045830b4a1debaf491cab593e96
haykelmhadhbi <mhadhbi.haikel@gmail.com>
2022-12-10 15:23:26 +0100

    modif

--- src/app/dashboard/components/gendre/gendre.component.ts
+++ src/app/dashboard/components/gendre/gendre.component.ts
@@ -7,57 +7,62 @@ import { ServiceStudentService } from '../../service/service-student.service';
   styleUrls: ['./gendre.component.css']
 })
 export class GendreComponent implements OnInit {
-  gen : String  ="Male"
-  studentBysexe : any []
-  Gendre : any []=[]
-  empSelected: any ;
+  Genre : any [];
+  val : any ;
+  studentBySexe : any []
   page:number=1;
 count:number=0;
 tableSize:number=5;
 tablesSizes:any=[1,2,3,4];
 titles="pagination"
 
+
+  name : any ="Male"
+  empSelected: any ;
+
+
   constructor(private studentService: ServiceStudentService) { }
 
   ngOnInit(): void {
-    this.getStudentBygendre()
-
-    this.Gendre=[
-      {gend:"Female"},
-      {gend:"Male"},
-      {gend:"Other"}
-     
-
+    this.getStudentByGender()
+    this.Genre=[
+      {gendr:"Male"},
+      {gendr:"Female"},
+      {gendr:"Other"}
     ]
+
+    this.studentBySexe
   }
 
 
-    getStudentBygendre (){
-      this.studentService.getEtudiantBySexe(this.studentwithsexe.gend).subscribe(
-        (res: any)=>{
-          console.log("liste by sexe");
-          this.studentBysexe=res;
-          console.log( this.studentBysexe);
-  
-        },
-        (erros:any)=>{
-          console.log("errors fdi sexe")
-          console.log(erros);
-  
-        }
-      )
-  
-      }
-      onGendreSelected(val:any){
 
-        this.studentwithsexe.gend=val;
-        console.log("jasa")
-        console.log(this.studentwithsexe.gend)
+getStudentByGender (){
+  this.studentService.getEtudiantBySexe(this.studentwithgendre.gendr).subscribe(
+    (res:any)=>{
+      console.log("cbon sexe")
+      console.log(res)
+      this.studentBySexe=res;
+    },
+    (erro:any)=>{
+      console.log("chay sexe")
+      console.log(erro)
+
+
+    }
+  )
   
-      }
-      studentwithsexe  ={
-        gend : ""
-    
-      }
 
 }
+onGendreSelected(val:any){
+
+  this.studentwithgendre.gendr=val;
+  console.log("jasa")
+  console.log(this.studentwithgendre.gendr)
+
+}
+studentwithgendre  ={
+  gendr : ""
+
+}
+
+ }
